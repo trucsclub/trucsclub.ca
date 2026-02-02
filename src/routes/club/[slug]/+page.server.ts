10602f7cbf2019f642ecf488f7500bf6f26e7583
Aidan <aidanjosiah02@gmail.com>
2026-02-02 10:55:12 -0800

    club correction

--- src/routes/club/[slug]/+page.server.ts
+++ src/routes/club/[slug]/+page.server.ts
@@ -1,3 +1,4 @@
+import clubs from '$lib/data/clubs.json'
 import { error } from '@sveltejs/kit';
 import type { ClubKey } from '$lib/types/club';
 import { getEvents } from '$lib/server/calendar';
@@ -7,7 +8,11 @@ export async function load({ params }) {
 	const slug = params.slug as ClubKey;
 
 	try {
-		const rawEvents = await getEvents(slug);
+		const calendarId = clubs[slug].calendarId;
+		if (!calendarId) {
+			console.warn(`No calendarId for club: ${slug}`);
+		}
+		const rawEvents = await getEvents(calendarId);
 
 		const events = rawEvents.map((event) =>
 			convertEvent(event, slug)
