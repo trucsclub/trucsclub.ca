c92f24e7750d7572f096bb4e24d269ec75558675
tyler <tyler@dezeeuw.ca>
2026-02-03 00:03:16 -0800

    browser tab title should match page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,14 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
-	let { children } = $props();
+	let { children, data } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+<link rel="icon" href={favicon} />
+<title>
+		{data?.heroData?.title || data?.hero?.title || 'TRU Computing'}
+	</title>
+</svelte:head>
 <ModeWatcher />
 {@render children()}
